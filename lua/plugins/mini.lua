4c99a15268dce67cc2911a278c6bd2ef0a20b843
Ediberto Júnior <ediberto.junior@fortics.com.br>
2024-11-11 17:34:08 -0300

    feat: adding more mini usefull modules

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,6 +5,13 @@ return {
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.pairs").setup()
 		require("mini.surround").setup()
+		require("mini.comment").setup()
+		require("mini.align").setup()
+		require("mini.animate").setup()
+		require("mini.diff").setup()
+		require("mini.git").setup()
+		require("mini.tabline").setup()
+
 		-- require("mini.files").setup()
 		local statusline = require("mini.statusline")
 		statusline.setup({ use_icons = vim.g.have_nerd_font })
